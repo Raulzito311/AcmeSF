022817e1b442dcabeacd4fe6d1670a5ae77aceca
Rodrigo Jorge <rodrigo@ssparisi.com.br>
2024-04-05 21:49:46 -0300

    modelos iniciais de Emprestimo e Forma de Pagamento na API de php

--- AcmeSF/api/src/models/Emprestimo.php
+++ AcmeSF/api/src/models/Emprestimo.php
@@ -1,4 +1,10 @@
 <?php
 class Emprestimo {
+    protected int $clienteId;
+	protected float $valorEmprestimo;
+	protected FormaDePagamento $formaDePagamento;
+	public DateTime $dataHora;
+	protected float $valorFinal;
+	protected array $parcelas;
 }
 ?>
