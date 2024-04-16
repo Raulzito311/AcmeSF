154dfc3cef1ac66caa0881858dfa42a323661876
Raulzito311 <raulmff@gmail.com>
2024-04-15 21:38:23 -0300

    Refactor Repository

--- AcmeSF/api/src/formaDePagamento/FormaDePagamentoRepositoryBDR.php
+++ AcmeSF/api/src/formaDePagamento/FormaDePagamentoRepositoryBDR.php
@@ -2,15 +2,10 @@
 require_once "vendor/autoload.php";
 
 class FormaDePagamentoRepositoryBDR implements FormaDePagamentoRepository {
-    protected PDO $pdo;
-
-    function __construct(){
-        $this->pdo = Connection::get();
-    }
 
     public function buscarPeloId($id): ?FormaDePagamento {
         try{
-            $ps = $this->pdo->prepare('SELECT * FROM formas_de_pagamento WHERE id = ?');
+            $ps = Connection::get()->prepare('SELECT * FROM formas_de_pagamento WHERE id = ?');
             $ps->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'FormaDePagamento');
             $ps->execute([$id]);
     
@@ -22,7 +17,7 @@ class FormaDePagamentoRepositoryBDR implements FormaDePagamentoRepository {
 
     public function buscarTodos():array {
         try{
-            $ps = $this->pdo->prepare('SELECT * FROM formas_de_pagamento');
+            $ps = Connection::get()->prepare('SELECT * FROM formas_de_pagamento');
             $ps->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'FormaDePagamento');
             $ps->execute();
             
