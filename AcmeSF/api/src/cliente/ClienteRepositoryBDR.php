4f835bc7a583f0b27a5f513f3646dfe967788870
Rodrigo Jorge <rodrigo@ssparisi.com.br>
2024-04-18 18:12:18 -0300

    implementa função para buscar cliente por cpf no clienteRepositoryBDR e o teste da função

--- AcmeSF/api/src/cliente/ClienteRepositoryBDR.php
+++ AcmeSF/api/src/cliente/ClienteRepositoryBDR.php
@@ -2,7 +2,7 @@
 
 class ClienteRepositoryBDR implements ClienteRepository{
     protected PDO $pdo;
-    
+
     public function buscarTodos():array{
         try{
             $ps = Connection::get()->prepare('SELECT * FROM clientes');
@@ -11,7 +11,7 @@ class ClienteRepositoryBDR implements ClienteRepository{
             
             return $ps->fetchAll();
         }catch(Exception $e){
-            throw new RepositoryException('Erro ao consultar formas de pagamento | ' . $e->getMessage() . ' | ' . $e->getPrevious()->getMessage(), 500);
+            throw new RepositoryException('Erro ao consultar clientes | ' . $e->getMessage() . ' | ' . $e->getPrevious()->getMessage(), 500);
         }
     }
 
@@ -23,7 +23,19 @@ class ClienteRepositoryBDR implements ClienteRepository{
     
             return $ps->fetchObject(Cliente::class);
         }catch(Exception $e){
-            throw new RepositoryException('Erro ao consultar formas de pagamento' . $e->getMessage(), 500);
+            throw new RepositoryException('Erro ao consultar cliente' . $e->getMessage(), 500);
+        }
+    }
+
+    public function buscarPeloCPF($cpf): Cliente{
+        try{
+            $ps = Connection::get()->prepare('SELECT * FROM clientes WHERE cpf = ?');
+            $ps->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, Cliente::class);
+            $ps->execute([$cpf]);
+    
+            return $ps->fetchObject(Cliente::class);
+        }catch(Exception $e){
+            throw new RepositoryException('Erro ao consultar cliente' . $e->getMessage(), 500);
         }
     }
 }
