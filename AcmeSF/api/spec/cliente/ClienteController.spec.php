d0cfcb22be07d655af297645f404ffd47f02b2f9
Rodrigo Jorge <rodrigo@ssparisi.com.br>
2024-04-20 20:48:22 -0300

    create test clienteController.spec.ph

---
+++ AcmeSF/api/spec/cliente/ClienteController.spec.php
@@ -0,0 +1,66 @@
+<?php
+
+use phputil\router\FakeHttpRequest;
+use phputil\router\FakeHttpResponse;
+
+require_once "vendor/autoload.php";
+
+describe('ClienteController', function() {
+    beforeAll(function() {
+        $sql = file_get_contents('dados.sql');
+        Connection::get()->exec($sql);
+    });
+    it('retrieves all Clientes', function() {
+        $req = new FakeHttpRequest();
+        $req->withMethod('GET');
+        $req->withUrl('/clientes');
+        $res = new FakeHttpResponse();
+
+        $app = App::getRouter();
+
+        $app->listen([ 'req' => $req, 'res' => $res ]);
+
+        expect($res->isStatus(200))->toBeTruthy();
+
+        $resObj = $res->dumpObject();
+
+        expect(empty($resObj->body))->toBeFalsy();
+    });
+    it('retrieves one Cliente by id', function() {
+        $req = new FakeHttpRequest();
+        $req->withMethod('GET');
+        $req->withUrl('/clientes/1');
+        $res = new FakeHttpResponse();
+
+        $app = App::getRouter();
+
+        $app->listen([ 'req' => $req, 'res' => $res ]);
+
+        expect($res->isStatus(200))->toBeTruthy();
+
+        $resObj = $res->dumpObject();
+
+        expect(empty($resObj->body))->toBeFalsy();
+
+        $cliente = json_decode($resObj->body[0]);
+
+        expect($cliente->id)->toBe(1);
+        expect($cliente->nome)->toBe('Rodrigo Jorge');
+        expect($cliente->cpf)->toBe('063.556.107-74');
+        expect($cliente->dataNascimento)->toBe('2003-02-12 00:00:00');
+    });
+    it('returns status "404 - Not Found" for an inexistant Cliente id', function() {
+        $req = new FakeHttpRequest();
+        $req->withMethod('GET');
+        $req->withUrl('/clientes/999');
+        $res = new FakeHttpResponse();
+
+        $app = App::getRouter();
+
+        $app->listen([ 'req' => $req, 'res' => $res ]);
+
+        expect($res->isStatus(404))->toBeTruthy();
+    });
+});
+
+?>
