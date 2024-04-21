005147f8020443b34b66f65492528c3bd23368b3
Rodrigo Jorge <rodrigo@ssparisi.com.br>
2024-04-20 22:22:42 -0300

    implements more tests in ClienteController.spec.php

--- AcmeSF/api/spec/cliente/ClienteController.spec.php
+++ AcmeSF/api/spec/cliente/ClienteController.spec.php
@@ -61,6 +61,41 @@ describe('ClienteController', function() {
 
         expect($res->isStatus(404))->toBeTruthy();
     });
+    it('retrieves one Cliente by CPF', function() {
+        $req = new FakeHttpRequest();
+        $req->withMethod('GET');
+        $req->withUrl('/clientes?cpf=063.556.107.74');
+        $res = new FakeHttpResponse();
+
+        $app = App::getRouter();
+
+        $app->listen([ 'req' => $req, 'res' => $res ]);
+
+        expect($res->isStatus(200))->toBeTruthy();
+
+        $resObj = $res->dumpObject();
+
+        expect(empty($resObj->body))->toBeFalsy();
+
+        $cliente = json_decode($resObj->body[0]);
+
+        expect($cliente->id)->toBe(1);
+        expect($cliente->nome)->toBe('Rodrigo Jorge');
+        expect($cliente->cpf)->toBe('063.556.107-74');
+        expect($cliente->dataNascimento)->toBe('2003-02-12');
+    });
+    it('returns status "404 - Not Found" for an inexistant Cliente CPf', function() {
+        $req = new FakeHttpRequest();
+        $req->withMethod('GET');
+        $req->withUrl('clientes?cpf=093.221.320-00');
+        $res = new FakeHttpResponse();
+
+        $app = App::getRouter();
+
+        $app->listen([ 'req' => $req, 'res' => $res ]);
+
+        expect($res->isStatus(404))->toBeTruthy();
+    });
 });
 
 ?>
