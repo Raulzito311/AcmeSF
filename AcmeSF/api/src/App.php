469602fbd0183d0f0d5e621694d13dc331d34263
Raulzito311 <raulmff@gmail.com>
2024-08-19 21:28:34 -0300

    Backend: Alterar permissoes para buscar usuário logado

--- AcmeSF/api/src/App.php
+++ AcmeSF/api/src/App.php
@@ -33,7 +33,7 @@ class App {
         $app
             ->use(cors(['origin' => 'http://localhost:5173', 'allowedHeaders' => 'content-type']))
                 ->route('/auth')
-                    ->get('/', function(HttpRequest $req, HttpResponse $res) {
+                    ->get('/', $precisaEstarLogado, function(HttpRequest $req, HttpResponse $res) {
                         $view = new AuthView($req, $res);
                         $controller = new AuthController($view, SessionFILE::get());
                         $controller->buscarUsuarioRegistrado();
