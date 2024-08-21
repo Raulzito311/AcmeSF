33b92197ab41f0b6e661de96f50035745b79a2fc
Raulzito311 <raulmff@gmail.com>
2024-08-21 17:37:36 -0300

    Apresentação

--- AcmeSF/api/spec/emprestimo/Emprestimo.spec.php
+++ AcmeSF/api/spec/emprestimo/Emprestimo.spec.php
@@ -2,6 +2,13 @@
 require_once "vendor/autoload.php";
 
 describe('Emprestimo', function() {
+    it("calculates ValorComJuros correctly", function() {
+
+        $emprestimo = new Emprestimo(1, new Cliente(), (new FormaDePagamento())->withJuros(0.1), '2024-08-21 17:31:22', 1000);
+        
+        expect($emprestimo->valorComJuros)->toBe(1100.00);
+    });
+
     it("creates Parcelas correctly", function() {
 
         $parcelas = Emprestimo::calcularParcelas(10000, (new FormaDePagamento())->withJuros(0.1)->withMeses(3));
